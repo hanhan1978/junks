42c2a4c8d496799435513ea3386fb6c69fefb0e2
富所 亮 <tomidokoror@bp.dev.istyle.local>
2013-10-02 15:50:01 +0900

    zuckerman improvment

--- ogiri/zuckerman.sh
+++ ogiri/zuckerman.sh
@@ -2,16 +2,8 @@
 
 for num in `seq $1`;
 do 
-  length=`expr length $num`
-  product=1
-  i=0;
-  while [ $i -lt $length ]
-    do
-    i=`expr $i + 1`
-    figure=`echo $num | cut -c $i`
-    product=`expr $product \* $figure`
-  done
-  
+  product=`echo $num | sed ":loop; /[0-9][0-9]/{ s/\([0-9]\)\([0-9]\)/\1 * \2/ ;b loop}" | sed "s/^/1 /" | sed "s/$/ * p/" | dc`;
+
   if [ 0 -eq $product ] ; then
     continue;
   elif [ 0 -eq `expr $num % $product` ] ; then
