51bcd67eaf2c26ff62997b71a048b0047e55941e
富所 亮 <tomidokoror@bp.dev.istyle.local>
2013-10-02 15:54:27 +0900

    zuckerman improvment v2

--- ogiri/zuckerman.sh
+++ ogiri/zuckerman.sh
@@ -2,7 +2,7 @@
 
 for num in `seq $1`;
 do 
-  product=`echo $num | sed ":loop; /[0-9][0-9]/{ s/\([0-9]\)\([0-9]\)/\1 * \2/ ;b loop}" | sed "s/^/1 /" | sed "s/$/ * p/" | dc`;
+  product=`echo $num | sed -e ":loop; /[0-9][0-9]/{ s/\([0-9]\)\([0-9]\)/\1 * \2/ ;b loop}" -e "s/^/1 /" -e "s/$/ * p/" | dc`;
 
   if [ 0 -eq $product ] ; then
     continue;
